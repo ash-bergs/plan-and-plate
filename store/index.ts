34a6842e6bc1d75b58bb62bfc93a6d7331245a7b
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-07-22 14:15:18 -0400

    forgot to add the store in the last commit

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,24 @@
 import { atom } from 'jotai';
-import { Recipe } from '@/types/recipes';
+import { Recipe, AddRecipeFormData } from '@/types/recipes';
 
 export const recipesAtom = atom<Recipe[] | undefined>(undefined);
+
+const initialFormData: AddRecipeFormData = {
+  title: '',
+  description: '',
+  content: '',
+  summary: '',
+  ingredients: [{ amount: 0, unit: '', name: '' }],
+  steps: [''],
+  //difficulty: '',
+  //cleanupRating: 0,
+  prepTime: 0,
+  cookTime: 0,
+  servingSize: 0,
+  //draft: true,
+  //cuisine: '',
+  //category: '',
+};
+
+// we'll make an atom to track our form data...
+export const addRecipeFormDataAtom = atom<AddRecipeFormData>(initialFormData);
